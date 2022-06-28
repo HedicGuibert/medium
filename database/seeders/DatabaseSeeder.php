a9bdc0ca844e05e8405991b6510f65055f88a339
Tom <t.kalbe@h3hitema.fr>
2022-06-28 16:04:40 +0200

    Add Comment model,migration,factory,seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+<<<<<<< HEAD
         // Order is very important : most dependant models last, least dependant ones first.
         $this->call([
             UserSeeder::class,
             ArticleSeeder::class,
         ]);
+=======
+        \App\Models\User::factory(2)->create();
+        \App\Models\Article::factory(10)->create();
+        \App\Models\Comment::factory(20)->create();
+>>>>>>> 0a882cf (Add Comment model,migration,factory,seeder)
     }
 }
