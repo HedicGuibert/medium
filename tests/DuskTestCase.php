5a4a06e40f5a27de7a37c97a5251ffefb89c95fe
Hédic Guibert <guiberthedic@gmail.com>
2022-06-29 22:53:05 +0200

    Add CI

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -37,7 +37,7 @@ abstract class DuskTestCase extends BaseTestCase
         ])->unless($this->hasHeadlessDisabled(), function ($items) {
             return $items->merge([
                 '--disable-gpu',
-                'headless',
+                '--headless',
                 '--no-sandbox',
                 '--window-size=1920,1080',
             ]);
