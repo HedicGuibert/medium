0b2bbfabca9c3be53ae8ed0858afe74572562b23
Tom <t.kalbe@h3hitema.fr>
2022-06-30 21:44:41 +0200

    fix status Article migration + factory

--- database/migrations/2022_06_27_134536_create_articles_table.php
+++ database/migrations/2022_06_27_134536_create_articles_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->text('introduction');
             $table->text('body');
             $table->integer('like')->default(0);
-            $table->enum('status',['pending']);
+            $table->enum('status',['published', 'draft'])->default('published');
             $table->string('image')->nullable();
             $table->string('slug')->unique();
             $table->foreignId('user_id')->constrained("users")->onDelete('cascade');
