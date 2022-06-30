06b4b2809cb3a49ecc91a04526d178638e43370b
Hédic Guibert <guiberthedic@gmail.com>
2022-06-30 14:11:02 +0200

    Improve tests performance

--- tests/Browser/FavouriteTest.php
+++ tests/Browser/FavouriteTest.php
@@ -2,19 +2,15 @@
 
 namespace Tests\Browser;
 
+use Illuminate\Support\Facades\Auth;
 use Laravel\Dusk\Browser;
 
 class FavouriteTest extends AbstractBaseTest
 {
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
     protected function setUp(): void
     {
         parent::setUp();
-        $this->generateUsers();
+        Auth::logout();
         $this->generateArticles();
     }
 
@@ -28,47 +24,39 @@ class FavouriteTest extends AbstractBaseTest
     public function testFavoritesExist()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                ->assertNotPresent('.icon-heart2')
-                ->visit('/login')
-                ->type('@email', $this->simpleUser->email)
-                ->type('@password', 'simpleuser')
-                ->press('@submit')
+            $browser
+                ->loginAs($this->getSimpleUser())
                 ->visit('/')
-                ->assertPresent('.icon-heart2')
-                ->logout();
+                ->assertPresent('.icon-heart2');
         });
     }
 
     public function testFavoritesAdd()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/favourite')
-                ->type('@email', $this->simpleUser->email)
-                ->type('@password', 'simpleuser')
-                ->press('@submit')
+            $browser
+                ->loginAs($this->getEditorUser())
+                ->visit('/favourite')
                 ->assertSee("Aucun article n'a été trouvé")
                 ->visit('/')
                 ->press('@add_to_favorite_article_2')
                 ->visit('/favourite')
-                ->assertSee('Article 2')
-                ->logout();
+                ->assertSee('Article 2');
         });
     }
 
     public function testFavoritesRemote()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/login')
-            ->type('@email', $this->simpleUser->email)
-            ->type('@password', 'simpleuser')
-            ->press('@submit')
-            ->press('@add_to_favorite_article_2')
-            ->visit('/favourite')
-            ->assertSee("Article 2")
-            ->press('@add_to_favorite_article_2')
-            ->visit('/favourite')
-            ->assertSee("Aucun article n'a été trouvé");
+            $browser
+                ->loginAs($this->getEditorUser())
+                ->visit('/')
+                ->press('@add_to_favorite_article_2')
+                ->visit('/favourite')
+                ->assertSee("Article 2")
+                ->press('@add_to_favorite_article_2')
+                ->visit('/favourite')
+                ->assertSee("Aucun article n'a été trouvé");
         });
     }
 }
